6312fdf144229a85e9eaf12c1a5f470d4d53f711
Fenysk <minefenysk@gmail.com>
2024-01-17 15:37:04 +0100

    💄 Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,4 +31,14 @@ export class AppComponent implements OnInit {
         }
     }
 
+    formatDate(date: Date) {
+        return new Date(date).toLocaleDateString('fr-FR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric'
+        });
+    }
+
 }
