d68c95506e1f58b4555d125e9e6d62c1e7d14dcd
Fenysk <minefenysk@gmail.com>
2024-01-17 14:13:10 +0100

    ✨ Pokémon list (with ngOnInit and @for)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [CommonModule, RouterOutlet],
-    template: `
-    <h1>Hello {{title}} !</h1>
-
-    <router-outlet></router-outlet>
-  `,
+    templateUrl: 'app.template.html',
     styles: [],
 })
-export class AppComponent {
-    title = 'world';
+export class AppComponent implements OnInit {
+    pokemonList: Pokemon[] = POKEMONS;
+
+    ngOnInit() {
+        console.table(this.pokemonList);
+    }
+
+    selectPokemon(pokemon: Pokemon) {
+        console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}.`);
+    }
+
 }
